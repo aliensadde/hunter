1eb412f66db7d095c7b0290c784bd9983495b50b
Aliensadde <vladyomkin@gmail.com>
2018-05-15 19:03:30 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/aliensadde/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    ca3e7502e7b13f1797ac8a5ff8d6ffd5e1ea9d3e
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
